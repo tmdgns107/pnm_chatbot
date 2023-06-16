d25c1bb05a2d96bba6fa2f3a00807701fe965af4
이승훈 <seunghun@iseunghun-ui-MacBookPro.local>
2023-06-17 00:36:09 +0900

    log 추가

--- index.ts
+++ index.ts
@@ -17,6 +17,8 @@ export const handler: Handler = async (event: APIGatewayProxyEvent): Promise<API
             return sendResponse(102, [], 'message is required.');
         }
 
+        console.log("typeof messages", typeof messages);
+        console.log("messages", messages);
         let returnMessages = await util.callGptAPI(messages);
         console.log("returnMessages", returnMessages);
 
